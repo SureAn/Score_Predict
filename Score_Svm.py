aaed7103d979cb668fc9062dc9b1359d581984ba
songshixin <18701637129@163.com>
2017-02-28 20:46:03 +0800

    add accuracy calculation
    add accuracy calculation  of "0" lable to parameter tuning

--- Score_Svm.py
+++ Score_Svm.py
@@ -16,11 +16,11 @@ trainYreader = np.loadtxt(open("train_data_Y.csv","rb"),delimiter=",",skiprows=0
 y = trainYreader[:,1]
 
 # normalize the data attributes
-normalized_X = preprocessing.normalize(X)
+#normalized_X = preprocessing.normalize(X)
 # standardize the data attributes
-standardized_X = preprocessing.scale(X)
+#standardized_X = preprocessing.scale(X)
 
-model = SVC(C=35,class_weight={0:2},gamma=0.03)
+model = SVC(C=30,class_weight={0:7},gamma=0.03)
 model.fit(X, y)
 print(model)
 trainY1reader = np.loadtxt(open("test_data_Y.csv","rb"),delimiter=",",skiprows=0)
@@ -31,9 +31,20 @@ X1 = trainX1reader[:,1:8]
 predicted = model.predict(X1)
 print(predicted)
 np.savetxt('predict.csv', predicted, delimiter = ',')
+#计算整体准确率
 accuracy = metrics.accuracy_score(y1, predicted)
 print('accuracy: %.2f%%' % (100 * accuracy))
 
+#计算“0”样本的准确率
+count_y=0
+count_P=0
+for j in range(1,298):
+    if y1[j]==0:
+        count_y+=1
+        if predicted[j]==0:
+            count_P+=1
+accuracy_really = count_P/count_y
+print('accuracy_really: %.2f%%' % (100 * accuracy_really))
 #lw = 2
 #plt.scatter(X1, y1, color='darkorange', label='data')
 #plt.hold('on')
@@ -43,5 +54,3 @@ print('accuracy: %.2f%%' % (100 * accuracy))
 #plt.title('Support Vector Regression')
 #plt.legend()
 #plt.show()
-#print(metrics.classification_report(expected, predicted))
-#print(metrics.confusion_matrix(expected, predicted))
