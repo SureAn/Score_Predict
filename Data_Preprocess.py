4bc2f6e15ea3baf65a45eb624dd9562ec0ff8ceb
songshixin <18701637129@163.com>
2017-03-06 21:03:38 +0800

    Data preprocess  improve
    1.Try three ways of data preprocess ： MinMaxScaler、scale、normalize
    2.Find scale is the best choice
    3.Improve the algorithm parameter tuning

--- Data_Preprocess.py
+++ Data_Preprocess.py
@@ -2,5 +2,5 @@ from sklearn import preprocessing
 
 #data preprocess :standardize and normalize the data
 def data_preprocess(X):
-    X = preprocessing.normalize(preprocessing.scale(X))
+    X = preprocessing.scale(X)
     return X
--- Score_Svm.py
+++ Score_Svm.py
@@ -11,26 +11,16 @@ from Test_Analysis import *
 X = load_data("train_data_X.csv")
 y = load_data("train_data_Y.csv")
 #train data preprocess
-#X = data_preprocess(X)
-#y = data_preprocess(y)
+X = data_preprocess(X)
 #fit support vector machine,return model
-model = train_svm(X,y,30,0.01)
+model = train_svm(X,y,3,0.01)
 #load text data
 y1 = load_data("test_data_Y.csv")
 X1 = load_data("test_data_X.csv")
 #test data preprocess
-#X1 = data_preprocess(X)
-#y1 = data_preprocess(y)
+X1 = data_preprocess(X1)
+
 #test data predict
 predicted = predict_data(model,X1)
 #analysis
 test_analysis(predicted,y1)
-#lw = 2
-#plt.scatter(X1, y1, color='darkorange', label='data')
-#plt.hold('on')
-#plt.plot(X1, predicted, color='navy',  label='RBF model')
-#plt.xlabel('data')
-#plt.ylabel('target')
-#plt.title('Support Vector Regression')
-#plt.legend()
-#plt.show()
